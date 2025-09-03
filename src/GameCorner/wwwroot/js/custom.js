5b7d850a2ebb7de18290246959798ffa7cff9113
veradisluna <lunajdavies@gmail.com>
2025-09-03 23:49:44 +0100

    Started work on mini crossword stuff!

--- src/GameCorner/wwwroot/js/custom.js
+++ src/GameCorner/wwwroot/js/custom.js
@@ -1,1 +1,13 @@
 ﻿window.setInputValue = (el, val) => { if (el) el.value = val; };
+
+window.lunaFocus = {
+    focus: function (id) {
+        const el = document.getElementById(id);
+        if (el) {
+            el.focus();
+            // optional: keep caret sane and avoid scroll jumps
+            el.setSelectionRange?.(1, 1);
+            el.scrollIntoView?.({ block: 'nearest', inline: 'nearest' });
+        }
+    }
+};
